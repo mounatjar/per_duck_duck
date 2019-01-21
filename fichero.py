c4477baebe8c15ef7f9d6914575d18428cef13c5
Mounat <m.eljarmouni.2018@alumnos.urjc.es>
2019-01-21 16:24:25 +0100

    hola

--- fichero.py
+++ fichero.py
@@ -1,3 +1,14 @@
 import sys
 
-def 
+lista = ["Mounat", "Paola", "Sofia"]
+numero = int(input("Introduzca el número del jugador: "))
+
+def duck(lista,numero):
+    if numero <= len(lista):
+        jugador = lista[numero + 1]
+        return jugador
+    else:
+        return "No hay tantos jugadores"
+print("El resultado es: ", duck(lista,numero))
+
+#hola q tal    
